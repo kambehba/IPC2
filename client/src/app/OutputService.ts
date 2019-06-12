eca55a33495acb09fd633b3ebde58e4a87bf3aa5
Kam Parsen <kamparsen@yahoo.com>
2019-06-12 14:19:39 -0400

    added empty service file

---
+++ client/src/app/OutputService.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Subject } from "rxjs";
+import { map } from "rxjs/operators";
+import { environment } from "../environments/environment";
+import { stringify } from "@angular/compiler/src/util";
+
+
+
+@Injectable({ providedIn: "root" })
+export class OutputService {
+
+    
+      
+  }
+
+  
+
+
+  
+
+  
+
